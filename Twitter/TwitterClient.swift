0f5c46ca4c898dd5aa499d77a699c08fdf79c858
dlangner5023 <dlangner@uw.edu>
2016-02-08 17:27:05 -0800

    finished first video

---
+++ Twitter/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  Twitter
+//
+//  Created by Dustin Langner on 2/8/16.
+//  Copyright © 2016 Dustin Langner. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1SessionManager {
+
+}
