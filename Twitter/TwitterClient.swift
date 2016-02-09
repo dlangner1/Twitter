f798ac37b77ae54a3f96521071e9f5f9256f9203
dlangner5023 <dlangner@uw.edu>
2016-02-08 17:29:45 -0800

    finished first video for monday night

--- Twitter/TwitterClient.swift
+++ Twitter/TwitterClient.swift
@@ -7,7 +7,21 @@
 //
 
 import UIKit
+import BDBOAuth1Manager
+
+let twitterConsumerKey = "ACX3k1YO9HpVUrs58PO7b5w1A"
+let twitterBaseURL = NSURL(string: "https://api.twitter.com")
+let twitterConsumerSecret = "fPO3zyIIGalbFLADSZnaxUque4cKn3UKklr1lckbobncNyI6Uu"
 
 class TwitterClient: BDBOAuth1SessionManager {
+    
+    class var sharedInstance: TwitterClient {
+        struct Static {
+            static let instance = TwitterClient(baseURL: twitterBaseURL, consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret)
+            
+        
+        }
+        return Static.instance
+    }
 
 }
